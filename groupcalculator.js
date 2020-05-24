397450984fa4d95b83824672a210a63e541be8c5
JonasGroenbek <jonasgroenbek@gmail.com>
2020-05-25 00:44:54 +0200

    _

--- groupcalculator.js
+++ groupcalculator.js
@@ -7,10 +7,10 @@ module.exports = {
 
 const groupSize = (p, g) => {
     const group = new Set()
-    for(let i = BigInt(0); i < p-BigInt(1); i++){
+    for(let i = 0; i < p-1; i++){
         group.add((g**i)%p)
     }
-    console.log(group.size)
+    return group.size
 }
 
-groupSize(BigInt(83), BigInt(2))
+console.log(groupSize(83, 2))
